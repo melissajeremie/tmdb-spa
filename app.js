93dbc9175dca50f89df32eb4f3bdc67cf62285a6
Melissa Jeremie <melissajeremie@gmail.com>
2022-04-13 12:33:46 -0400

    search button

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("js connected");
+const API_KEY = 'b0738a033f97cb8789236ad33e1812e7';
+
+const searchButton = document.querySelector('#search');
+const input = document.querySelector('#inputValue');
+
+searchButton.onclick = function(event) {
+    event.preventDefault();
+    const value = input.value;
+    console.log('Entered: ', value);
+
+}
