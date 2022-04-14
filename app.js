f41734e927e28a12369b1eb96270a896a4c758cb
Melissa Jeremie <melissajeremie@gmail.com>
2022-04-13 22:36:03 -0400

    styling and pop search

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const API_KEY = 'b0738a033f97cb8789236ad33e1812e7';
 const url = 'https://api.themoviedb.org/3/search/movie?api_key=b0738a033f97cb8789236ad33e1812e7&language=en-US&page=1&query=';
+const popUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w500'
 
 const searchButton = document.querySelector('#search');
@@ -7,7 +8,13 @@ const input = document.querySelector('#inputValue');
 const popular = document.querySelector('#popular');
 const searchResults = document.querySelector('#search-results');
 
-window.onload = function popularMovies()
+window.onload = function(data) {
+    fetch(popUrl) 
+    .then((res) => res.json())
+    .then(getPopularMovies)
+    .catch((error) => {
+        console.log('Error: ', error)
+})};
 
 function movieLayout(movies){
     return movies.map((movie) => {
@@ -26,6 +33,9 @@ function createMovieContainer(movies) {
     <section class="section">
     ${movieLayout((movies))}
     </section>
+    <div class="content">
+    <p id="content-close">x</p>
+    </div>
     `;
 
     movieElement.innerHTML = movieTemplate;
@@ -38,6 +48,13 @@ function renderSearchResults(data) {
     const movies = data.results;
         const movieBlock = createMovieContainer(movies);
         searchResults.appendChild(movieBlock)
+        console.log('Data: ', data);
+}
+
+function getPopularMovies(data) {
+    const movies = data.results;
+        const movieBlock = createMovieContainer(movies);
+        popular.appendChild(movieBlock)
         console.log('Data: ', data);
 }
 searchButton.onclick = function(event) {
@@ -54,3 +71,4 @@ searchButton.onclick = function(event) {
     });
 input.value = '';
 }
+
